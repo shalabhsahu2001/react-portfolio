3f5d3f07e48407cb336ee0efbe16787a29e09e52
Shalabh Sahu <sahushalabh2001@gmail.com>
2024-07-20 00:18:26 +0530

    nav bar logic and styling

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,7 +1,9 @@
 import './index.scss'
-import {Link} from 'react-router-dom'
+import {Link, NavLink} from 'react-router-dom'
 import LogoS from '../../assets/images/logo-s.png';
 import LogoSubtitle from '../../assets/images/logo_sub.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
 
 const Sidebar = () => (
     <div className='nav-bar'>
@@ -9,7 +11,17 @@ const Sidebar = () => (
             <img src = {LogoS} alt = 'logo' />
             <img className = 'sub-logo' src = {LogoSubtitle} alt = 'shalabh' />
         </Link>
-
+        <nav>
+            <NavLink exact = "true" activeClassname = "active" to="/">
+                <FontAwesomeIcon icon={faHome} color='#4d4d4e'/>
+            </NavLink>
+            <NavLink exact = "true" activeClassname = "active" className = "about-link" to="/about">
+                <FontAwesomeIcon icon={faUser} color='#4d4d4e'/>
+            </NavLink>
+            <NavLink exact = "true" activeClassname = "active" className="contact-link" to="/contact">
+                <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e'/>
+            </NavLink>
+        </nav>
     </div>
 )
 
