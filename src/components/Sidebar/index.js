3f51b83fc005ac92a88044c8997415098ffb278b
Shalabh Sahu <sahushalabh2001@gmail.com>
2024-07-22 23:50:01 +0530

    fixed sub logo

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,7 +1,7 @@
 import './index.scss'
 import {Link, NavLink} from 'react-router-dom'
 import LogoS from '../../assets/images/logo-s.png';
-import LogoSubtitle from '../../assets/images/logo_sub.png';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
 import { faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons';
@@ -10,7 +10,7 @@ const Sidebar = () => (
     <div className='nav-bar'>
         <Link className='logo' to = '/'>
             <img src = {LogoS} alt = 'logo' />
-            <img className = 'sub-logo' src = {LogoSubtitle} alt = 'shalabh' />
+            
         </Link>
         <nav>
             <NavLink exact = "true" activeClassname = "active" to="/">
